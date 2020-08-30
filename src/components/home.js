ecf02213e43a11c54116d4544922b2e3d896b257
Vijay-Giri <lit2016038@iiitl.ac.in>
2020-08-30 20:16:30 +0530

    profile pic

--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,7 @@ import Footer from './footer.js'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import profile_pic from './../images/profile_pic.jpeg';
+import profile_pic from './../images/dp.jpeg';
 
 class Home extends Component {
     render() {
@@ -17,7 +17,7 @@ class Home extends Component {
                     </Row>
                     <Row className="border-top justify-content-between p-3">
                         <Col className="p-0">
-                            <img src={profile_pic} alt="profile-pic" width="420px" height="650px"/>
+                            <img src={profile_pic} alt="profile-pic" width="600px" height="600px"/>
                         </Col>
 
                     </Row>

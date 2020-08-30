181dd264c55462845871d7fdb85ab1e24bc2d8e6
Vijay-Giri <lit2016038@iiitl.ac.in>
2020-08-30 19:33:20 +0530

    css changes in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import styles from './../styles/footer.css';
 import {
     faFacebook,
@@ -11,32 +14,36 @@ import {
 class Footer extends Component {
     render() {
         return (
-            <div className={styles.footerBody}>
-                <a
-                    href="https://www.linkedin.com/in/vijay-giri-11b1b4154/"
-                    target="_blank"
-                >
-                    <FontAwesomeIcon icon={faLinkedin} size="1x" />
-                </a>
-                <a
-                    href="https://github.com/Vijay-Giri"
-                    target="_blank"
-                >
-                    <FontAwesomeIcon icon={faGithub} size="1x" />
-                </a>
-                <a
-                    href="https://www.facebook.com/girivijay008"
-                    target="_blank"
-                >
-                    <FontAwesomeIcon icon={faFacebook} size="1x" />
-                </a>
-                <a
-                    href="https://www.instagram.com/girivijay008/"
-                    target="_blank"
-                >
-                    <FontAwesomeIcon icon={faInstagram} size="1x" />
-                </a>
-            </div>
+            <Container fluid={true}>
+                <Row className="border-top justify-content-between p-3">
+                    <Col className="p-0">
+                        <a
+                            href="https://www.linkedin.com/in/vijay-giri-11b1b4154/"
+                            target="_blank"
+                        >
+                            <FontAwesomeIcon icon={faLinkedin} size="1x" />
+                        </a>
+                        <a
+                            href="https://github.com/Vijay-Giri"
+                            target="_blank"
+                        >
+                            <FontAwesomeIcon icon={faGithub} size="1x" />
+                        </a>
+                        <a
+                            href="https://www.facebook.com/girivijay008"
+                            target="_blank"
+                        >
+                            <FontAwesomeIcon icon={faFacebook} size="1x" />
+                        </a>
+                        <a
+                            href="https://www.instagram.com/girivijay008/"
+                            target="_blank"
+                        >
+                            <FontAwesomeIcon icon={faInstagram} size="1x" />
+                        </a>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }

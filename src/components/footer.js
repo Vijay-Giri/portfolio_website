b3031dd58a8c28507cc92aeecdc01aec6492a4f3
Vijay-Giri <lit2016038@iiitl.ac.in>
2020-08-30 18:26:55 +0530

    css footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,37 +1,33 @@
 import React, { Component } from 'react';
-import Container from 'react-bootstrap/Container'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './../styles/footer.css';
 import {
     faFacebook,
     faInstagram,
-    faLinkedin,
-    faGmail
+    faLinkedin
 } from '@fortawesome/free-brands-svg-icons';
 
 class Footer extends Component {
     render() {
         return (
-            <div className="ml-auto">
+            <div className={styles.footerBody}>
                 <a
                     href="https://www.linkedin.com/in/vijay-giri-11b1b4154/"
-                    className="mr-2"
                     target="_blank"
                 >
-                    <FontAwesomeIcon icon={faLinkedin} size="1x" />
+                    <FontAwesomeIcon icon={faLinkedin} size="2x" />
                 </a>
                 <a
                     href="https://www.facebook.com/girivijay008"
-                    className="mr-2"
                     target="_blank"
                 >
-                    <FontAwesomeIcon icon={faFacebook} size="1x" />
+                    <FontAwesomeIcon icon={faFacebook} size="2x" />
                 </a>
                 <a
                     href="https://www.instagram.com/girivijay008/"
-                    className="mr-2"
                     target="_blank"
                 >
-                    <FontAwesomeIcon icon={faInstagram} size="1x" />
+                    <FontAwesomeIcon icon={faInstagram} size="2x" />
                 </a>
             </div>
         );

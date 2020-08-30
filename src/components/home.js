ed07a13507d9488d84e18246b79ddae287bebb7f
Vijay-Giri <lit2016038@iiitl.ac.in>
2020-08-30 20:02:04 +0530

    homepage

--- src/components/home.js
+++ src/components/home.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import './../styles/home.css';
 import Header from './header.js'
 import Footer from './footer.js'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import profile_pic from './../images/profile_pic.jpeg';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <Header />
-                <Footer />
+                <Container fluid={true}>
+                    <Row>
+                        <Header />
+                    </Row>
+                    <Row className="border-top justify-content-between p-3">
+                        <Col className="p-0">
+                            <img src={profile_pic} alt="profile-pic" width="420px" height="650px"/>
+                        </Col>
+
+                    </Row>
+                    <Row>
+                        <Footer />
+                    </Row>
+                </Container>
             </div>
         );
     }
